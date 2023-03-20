652b006fb94102705cf69290ff5edf92eb72ed82
AYAM Kenny <kenny.ayam@outlook.fr>
2023-03-20 18:55:29 +1100

    commit

--- src/app/boite/boite.component.ts
+++ src/app/boite/boite.component.ts
@@ -10,32 +10,32 @@ export class BoiteComponent {
   tirelire_2:number = 0;
   tirelire_3:number = 0;
 
-    depenser(numeroTirelire: number) {
-      switch (numeroTirelire) {
-        case 1:
-          this.tirelire_1 = 0;
-          break;
-        case 2:
-          this.tirelire_2 = 0;
-          break;
-        case 3:
-          this.tirelire_3 = 0;
-          break;
-      }
+  depenser(numeroTirelire: number) {
+    switch (numeroTirelire) {
+      case 1:
+        this.tirelire_1 = 0;
+        break;
+      case 2:
+        this.tirelire_2 = 0;
+        break;
+      case 3:
+        this.tirelire_3 = 0;
+        break;
     }
+  }
 
-    ajouter() {
-      let numeroTirelire = Math.floor(Math.random()*3)+1;
-      switch (numeroTirelire) {
-        case 1:
-        this.tirelire_1 += 100;
-        break;
-        case 2:
-        this.tirelire_2 += 100;
-        break;
-        case 3:
-        this.tirelire_3 += 100;
-        break;
-      }
+  ajouter() {
+    let numeroTirelire = Math.floor(Math.random()*3)+1;
+    switch (numeroTirelire) {
+      case 1:
+      this.tirelire_1 += 100;
+      break;
+      case 2:
+      this.tirelire_2 += 100;
+      break;
+      case 3:
+      this.tirelire_3 += 100;
+      break;
     }
+  }
 }
